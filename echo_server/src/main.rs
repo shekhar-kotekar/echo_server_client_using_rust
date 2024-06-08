a11e699be22d71669bae19d2913175090cfeb1ac
chandrashekhar-kotekar <chandrashekhar.kotekar@schibsted.com>
2024-06-08 14:54:04 +0200

    client receives message from echo server

--- echo_server/src/main.rs
+++ echo_server/src/main.rs
@@ -1,4 +1,8 @@
+use std::io::Read;
+use std::io::Write;
 use std::net::TcpListener;
+
+use std::net::TcpStream;
 
 const ECHO_SERVER_ADDRESS: &str = "127.0.0.1:5056";
 fn main() {
@@ -12,5 +16,13 @@ fn main() {
             stream.peer_addr().unwrap().ip(),
             stream.peer_addr().unwrap().port()
         );
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut message = [0; 1024];
+    stream.read(&mut message).unwrap();
+    stream.write_all(&message).unwrap();
+    println!("echoed message: {}", String::from_utf8_lossy(&message));
 }
