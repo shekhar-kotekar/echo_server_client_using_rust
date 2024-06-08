3e9aa55d1ae218c8f251eb0bef3b8ef31b5f3cce
chandrashekhar-kotekar <chandrashekhar.kotekar@schibsted.com>
2024-06-08 15:27:20 +0200

    tracing enabled

--- echo_client/src/main.rs
+++ echo_client/src/main.rs
@@ -7,11 +7,12 @@ const ECHO_SERVER_ADDRESS: &str = "localhost:5056";
 
 #[tokio::main]
 async fn main() {
-    println!("Connecting to echo server at {}", ECHO_SERVER_ADDRESS);
+    start_tracing().await;
+    tracing::info!("Connecting to echo server at {}", ECHO_SERVER_ADDRESS);
     let result = TcpStream::connect(ECHO_SERVER_ADDRESS).await;
     match result {
         Ok(mut stream) => {
-            println!(
+            tracing::info!(
                 "Connected to echo server at {}:{}",
                 stream.local_addr().unwrap().ip(),
                 stream.local_addr().unwrap().port()
@@ -20,16 +21,28 @@ async fn main() {
             let message = format!("Hello from {}", stream.local_addr().unwrap().port());
             match stream.write_all(message.as_bytes()).await {
                 Ok(_) => {
-                    println!("Sent: {}", message);
+                    tracing::info!("Sent: {}", message);
                     let mut buffer: String = String::new();
                     stream.read_to_string(&mut buffer).await.unwrap();
-                    println!("Received: {}", buffer);
+                    tracing::info!("Received: {}", buffer);
                 }
                 Err(e) => {
-                    println!("Error sending message: {}", e);
+                    tracing::error!("Error sending message: {}", e);
                 }
             };
         }
-        Err(e) => println!("Error connecting to echo server: {}", e),
+        Err(e) => tracing::error!("Error connecting to echo server: {}", e),
     }
+}
+
+async fn start_tracing() {
+    let subscriber = tracing_subscriber::fmt::Subscriber::builder()
+        .with_max_level(tracing::Level::DEBUG)
+        .compact()
+        .with_file(true)
+        .with_line_number(true)
+        .with_target(false)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    tracing::info!("Tracing started");
 }
--- echo_server/src/main.rs
+++ echo_server/src/main.rs
@@ -7,12 +7,13 @@ const ECHO_SERVER_ADDRESS: &str = "127.0.0.1:5056";
 
 #[tokio::main]
 async fn main() {
-    println!("Echo server listening on {}", ECHO_SERVER_ADDRESS);
+    start_tracing().await;
+    tracing::info!("Echo server listening on {}", ECHO_SERVER_ADDRESS);
     let listener = TcpListener::bind(ECHO_SERVER_ADDRESS).await.unwrap();
 
     loop {
         let (stream, _) = listener.accept().await.unwrap();
-        println!(
+        tracing::info!(
             "Connection accepted from {}:{}",
             stream.peer_addr().unwrap().ip(),
             stream.peer_addr().unwrap().port()
@@ -25,5 +26,17 @@ async fn handle_connection(mut stream: TcpStream) {
     let mut message = [0; 1024];
     stream.read(&mut message).await.unwrap();
     stream.write_all(&message).await.unwrap();
-    println!("sent: {}", String::from_utf8_lossy(&message));
+    tracing::info!("sent: {}", String::from_utf8_lossy(&message));
+}
+
+async fn start_tracing() {
+    let subscriber = tracing_subscriber::fmt::Subscriber::builder()
+        .with_max_level(tracing::Level::DEBUG)
+        .compact()
+        .with_file(true)
+        .with_line_number(true)
+        .with_target(false)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    tracing::info!("Tracing started");
 }
